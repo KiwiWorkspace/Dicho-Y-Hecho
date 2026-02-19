d3771356794ae93d2a9018b24c1de33c6f04ab7d
milian-rm <rmilian-2021493@kinal.edu.gt>
2026-02-19 14:35:21 -0600

    Datos de areas verdes

--- BD/DichoYHecho.sql
+++ BD/DichoYHecho.sql
@@ -9,11 +9,26 @@ create table usuario(
     edad_usuario int not null
 );
 
+
 create table areas_verdes(
 id_area int auto_increment not null primary key,
-nombre_area varchar(30) not null,
+nombre_area varchar(60) not null,
 tipo_area varchar(30) not null,
 ubicacion_area varchar(50) not null,
-estado_area varchar(30) not null,
-tipo_therian varchar(15) not null
+estado_area varchar(30) not null
 );
+INSERT INTO areas_verdes (nombre_area, tipo_area, ubicacion_area, estado_area) VALUES
+('Parque Naciones Unidas', 'Parque Nacional', 'Villa Nueva / Amatitlán', 'Excelente'),
+('Parque Ecológico Deportivo Cayalá', 'Reserva Ecológica', 'Zona 16', 'Bueno'),
+('Parque Erick Barrondo', 'Parque Polideportivo', 'Zona 7', 'Regular'),
+('Parque Jungla Urbana', 'Parque Ecológico', 'Zona 15', 'Bueno'),
+('Cerrito del Carmen', 'Parque Histórico', 'Zona 1', 'Excelente'),
+('Parque Ecológico Kanajuyú', 'Reserva Natural', 'Zona 16', 'Bueno'),
+('Parque Regional El Zapote', 'Parque Ecológico', 'Zona 2', 'Bueno'),
+('Parque Colón', 'Plaza Recreativa', 'Zona 1', 'Regular'),
+('Hipódromo del Norte', 'Bosque Urbano', 'Zona 2', 'Bueno'),
+('Jardín Botánico CECON', 'Jardín Botánico', 'Zona 10', 'Excelente'),
+('Parque Minerva', 'Parque Recreativo', 'Zona 2', 'Bueno'),
+('Barranco de Gerona', 'Corredor Ecológico', 'Zona 1', 'En Recuperación'),
+('Parque La Asunción', 'Parque Ecológico', 'Zona 5', 'Bueno'),
+('Parque de la Paz', 'Parque Deportivo', 'Zona 21', 'Nuevo');
