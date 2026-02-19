caf0a07a0dd16cb357878dc850ce981cafed636b
CampaRM <bcampa-2025434@kinal.edu.gt>
2026-02-19 17:15:32 -0600

    añadiendo atributos a administrador

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/entity/Administrador.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/entity/Administrador.java
@@ -1,4 +1,15 @@
 package com.dichoyhecho.dichoyhecho.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+    "idAdmin",
+    "nombre",
+    "apellidos",
+    "edad",
+    "direccion",
+    "contraseña"
+})
 public class Administrador {
 }
