7996b9329ab4046b92ff57263fbf043137e017bd
cristanders <cperez-2025597@kinal.edu.gt>
2026-02-19 13:48:19 -0600

    Entidad Usuario en BD

--- BD/DichoYHecho.sql
+++ BD/DichoYHecho.sql
@@ -1,1 +1,10 @@
 create database dichoyhecho_in5bv;
+use dichoyhecho_in5bv;
+create table usuario(
+	id_usuario int auto_increment not null primary key,
+    nombre_usuario varchar(60) not null,
+    apellido_usuario varchar(60) not null,
+    handle_usuario varchar(60) not null,
+    contrasena varchar(60) not null,
+    edad_usuario int not null
+);
