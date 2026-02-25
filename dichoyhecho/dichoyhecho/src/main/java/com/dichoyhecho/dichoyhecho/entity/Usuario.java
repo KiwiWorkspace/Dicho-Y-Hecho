336419c8340b5adc687a49a3df87d9c7e0e4441c
cristanders <cperez-2025597@kinal.edu.gt>
2026-02-24 23:38:21 -0600

    Se actualizo controller, service, repository y repositoryImpl de usuario

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/entity/Usuario.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/entity/Usuario.java
@@ -3,11 +3,14 @@ package com.dichoyhecho.dichoyhecho.entity;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @JsonPropertyOrder({
         "idUsuario",
         "nombreUsuario",
         "apellidoUsuario",
+        "handleUsuario",
+        "contrasena",
         "edadUsuario"
 })
 @Entity
@@ -19,18 +22,29 @@ public class Usuario {
     @Column(name = "id_usuario")
     private Integer idUsuario;
 
+    @NotBlank(message = "Debe ingresar un nombre ")
+    @Size(min = 2, max = 60, message = "Su nombre  debe tener 2 y 60 caracteres")
     @Column(name = "nombre_usuario")
     private String nombreUsuario;
 
+    @NotBlank(message = "Debe ingresar un apellido ")
+    @Size(min = 2, max = 60, message = "Su apellido  debe tener 2 y 60 caracteres")
     @Column(name = "apellido_usuario")
     private String apellidoUsuario;
 
+    @NotBlank(message = "Debe ingresar un apodo para su  usuario")
+    @Size(min = 3, max = 60, message = "Su apodo debe tener 3 y 60 caracteres")
     @Column(name = "handle_usuario")
     private String handleUsuario;
 
+    @NotBlank(message = "Debe ingresar una contrasena")
+    @Size(min = 8, max = 12, message = "La contraseña debe terner de 8 a 12 caracteres")
     @Column(name = "contrasena")
     private String contrasena;
 
+    @NotNull(message = "Debe ingresar una edad ")
+    @Min(value = 3, message = "La edad debe ser mayor a 3")
+    @Max(value = 135, message = "La edad debe ser menor a 135")
     @Column(name = "edad_usuario")
     private Integer edadUsuario;
 
--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/service/UsuarioServiceImpl.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/service/UsuarioServiceImpl.java
@@ -1,29 +1,53 @@
 package com.dichoyhecho.dichoyhecho.service;
 
 import com.dichoyhecho.dichoyhecho.entity.Usuario;
+import com.dichoyhecho.dichoyhecho.exception.ResourceNotFoundException;
+import com.dichoyhecho.dichoyhecho.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     public List<Usuario> listar() {
-        return List.of();
+        return usuarioRepository.findAll();
     }
 
     @Override
-    public void crear(Usuario usuario) {
-
+    public Usuario obtenerPorId(Integer id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario con Id no encontrado :" + id));
     }
 
     @Override
-    public int actualizar(Long id, Usuario usuario) {
-        return 0;
+    public Usuario crear(Usuario usuario) {
+        usuario.setIdUsuario(null);
+        return usuarioRepository.save(usuario);
     }
 
     @Override
-    public int eliminar(Long id) {
-        return 0;
+    public Usuario actualizar(Integer id, Usuario usuario) {
+        Usuario existente = obtenerPorId(id);
+        existente.setNombreUsuario(usuario.getNombreUsuario());
+        existente.setApellidoUsuario(usuario.getApellidoUsuario());
+        existente.setHandleUsuario(usuario.getHandleUsuario());
+        existente.setContrasena(usuario.getContrasena());
+        existente.setEdadUsuario(usuario.getEdadUsuario());
+
+        return usuarioRepository.save(existente);
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Usuario con ID no encontrado: " + id);
+        }
+        usuarioRepository.deleteById(id);
     }
 }
