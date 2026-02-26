9c41c392414abda30b7f74b1a705c721e116025b
CampaRM <bcampa-2025434@kinal.edu.gt>
2026-02-26 17:01:20 -0600

    No se

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/CategoriaProblema.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/CategoriaProblema.java
@@ -1,8 +1,21 @@
 package com.dichoyhecho.dichoyhecho.enums;
 
 public enum CategoriaProblema {
-    nivelBajo,
-    nivelModerado,
-    nivelAlto,
-    nivelCritico
+    nivel_Bajo("nivel bajo"),
+    nivelModerado("nivel moderado"),
+    nivelAlto("nivel alto"),
+    nivelCritico("nivel critico");
+
+    //Variable para guardar el texto en el enum)
+    private String descripcion;
+
+    //constructor para guardar el valor dentro de la variable interna
+    CategoriaProblema(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //obtenemos el valor de la variable interna
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
