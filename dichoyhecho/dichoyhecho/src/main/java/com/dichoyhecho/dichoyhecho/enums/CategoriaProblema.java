40e8e9fb5e8fffb1272934e4c3cad71daf7c6002
amax2019189 <amax-2019189@kinal.edu.gt>
2026-02-27 17:13:30 -0600

    Solución al problema de los ENums

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/CategoriaProblema.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/CategoriaProblema.java
@@ -1,12 +1,8 @@
 package com.dichoyhecho.dichoyhecho.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum CategoriaProblema {
-    nivel_Bajo,
-    nivelModerado,
-    nivelAlto,
-    nivelCritico;
-
+    NIVEL_BAJO,
+    NIVEL_MODERADO,
+    NIVEL_ALTO,
+    NIVEL_CRITICO
 }
--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/EstadoProblema.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/enums/EstadoProblema.java
@@ -1,38 +1,12 @@
 package com.dichoyhecho.dichoyhecho.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.util.Arrays;
-
 public enum EstadoProblema {
-    reportado("reportado"),
-    enInspeccionTecnica("En inspeccion tecnica"),
-    pendienteDeAprobacion("Pediente de aprobación"),
-    enRevision("En revisión"),
-    programado("Programado"),
-    enProceso("En proceso"),
-    resuelto("Resuelto"),
-    rechazado("rechazado");
-
-    //se crea una variable para guardar una constante (el texto que se guardara el enum)
-    private String estado;
-
-    //constructor para guardar el valor dentro de la variable (estado)
-    EstadoProblema(String estado) {
-        this.estado = estado;
-    }
-
-    //obtenemos el valor de la variable interna
-    @JsonValue
-    public String getEstado() {
-        return estado;
-    }
-
-    @JsonCreator
-    public static EstadoProblema descripcionEstado(String estado){
-        return Arrays.stream(values()).filter(c -> c.estado.equalsIgnoreCase(estado))
-                .findFirst().orElseThrow(() -> new IllegalArgumentException("La estado es invalido: " + estado));
-
-    }
+    REPORTADO,
+    EN_INSPECCION_TECNICA,
+    PENDIENTE_DE_APROBACION,
+    EN_REVISION,
+    PROGRAMADO,
+    EN_PROCESO,
+    RESUELTO,
+    RECHAZADO
 }
