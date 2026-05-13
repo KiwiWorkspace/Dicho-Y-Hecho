9bffa0d60dd5c639be2c7ba501bb8a81061416a7
cristanders <cperez-2025597@kinal.edu.gt>
2026-05-12 23:57:15 -0600

    Se agregaron bases de html y se arregló el contact y se dio nuevo estilo al profile

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/controller/ContactController.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/controller/ContactController.java
@@ -6,14 +6,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@RequestMapping("/contact")
 public class ContactController {
 
     @GetMapping("/contact")
-    public String login() {
+    public String showContact() {
         return "contact";
     }
 }
