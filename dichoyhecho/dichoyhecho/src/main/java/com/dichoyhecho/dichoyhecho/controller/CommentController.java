4438e50f617170ac6fb1391d12364ef8ea3d5ec0
cristanders <cperez-2025597@kinal.edu.gt>
2026-05-14 16:40:12 -0600

    Se crearon plantillas para headers del home y se arregló diseño del profile

--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/controller/CommentController.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/controller/CommentController.java
@@ -2,18 +2,27 @@ package com.dichoyhecho.dichoyhecho.controller;
 
 import com.dichoyhecho.dichoyhecho.dto.CommentRequest;
 import com.dichoyhecho.dichoyhecho.entity.Comments;
+import com.dichoyhecho.dichoyhecho.entity.Users;
+import com.dichoyhecho.dichoyhecho.repository.CommentRepository;
 import com.dichoyhecho.dichoyhecho.service.UserServiceImpl;
 import jakarta.validation.Valid;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/dichoyhecho/comentarios")
+@Controller
 public class CommentController {
+
+    @GetMapping("/comment")
+    public String ShowComments(){
+        return "comment";
+    }
+
     @Autowired
     private UserServiceImpl.CommentService commentService;
 
--- dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/repository/CommentRepository.java
+++ dichoyhecho/dichoyhecho/src/main/java/com/dichoyhecho/dichoyhecho/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.dichoyhecho.dichoyhecho.repository;
 import com.dichoyhecho.dichoyhecho.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comments, Long> {
+    List<Comments> findAllByOrderByCommentDateDesc();
+
 }
